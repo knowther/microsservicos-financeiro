4ec922b0e2640dd054248dd5bf71a306b27f2e67
Johnny Wesley <johnnywesley.contato@outlook.com>
2023-08-25 22:13:10 -0300

    arquitetura inicial do microserviço com eureka e o ms básico de clientes

---
+++ ms-clients/src/test/java/io/github/knwother/msclients/MsClientsApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.knwother.msclients;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsClientsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
